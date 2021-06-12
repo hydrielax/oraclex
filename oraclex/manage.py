fa07f5ee7d637dec9b5a3e24ebd3e73062d137b1
hydrielax <alexidelage@gmail.com>
2021-06-12 15:15:21 +0200

    recreate the site

--- oraclex/manage.py
+++ oraclex/manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
-ALLOWED_HOSTS = ['*']
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oraclex.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
