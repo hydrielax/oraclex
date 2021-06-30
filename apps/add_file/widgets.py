1f57f472ff8dff985c7497c8d36796ac75d651b0
Théodore <theodorechristianradu@gmail.com>
2021-07-01 00:01:41 +0200

    (Vrai) bouton de sélection des jugements

--- apps/add_file/widgets.py
+++ apps/add_file/widgets.py
@@ -6,9 +6,9 @@ class SimpleFileInput(FileInput):
     template_name = 'add_file/widgets/file_input.html'
     def __init__(self, attrs = None):
         super().__init__(attrs)
-        self.text = attrs['text']
+        self.text = attrs.get('text', '')
         self.id = attrs['id']
-    def get_context(self, name, value, attrs):
+    def get_context(self, name, value, attrs = None):
         context = super().get_context(name, value, attrs)
         context['widget'].update({'text': self.text, 'id': self.id})
         return context
