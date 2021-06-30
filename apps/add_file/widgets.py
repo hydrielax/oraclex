44366324a89f59ff505ff78c64f05ce100c3bcdd
Théodore <theodorechristianradu@gmail.com>
2021-07-01 01:17:14 +0200

    Dabest bouton de sélection des jugements

--- apps/add_file/widgets.py
+++ apps/add_file/widgets.py
@@ -1,14 +1,15 @@
 from django.forms import FileInput
-from django.utils.html import format_html
-
 
 class SimpleFileInput(FileInput):
+
     template_name = 'add_file/widgets/file_input.html'
-    def __init__(self, attrs = None):
-        super().__init__(attrs)
+
+    def __init__(self, *args, **kwargs):
+        attrs = kwargs.pop('attrs', {})
+        super().__init__(*args, **kwargs)
         self.text = attrs.get('text', '')
-        self.id = attrs['id']
-    def get_context(self, name, value, attrs = None):
+
+    def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context['widget'].update({'text': self.text, 'id': self.id})
+        context['widget'].update({'text': self.text, 'id': attrs['id']})
         return context
