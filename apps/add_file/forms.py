0c450d1d62d1976ac87d43b3efc5703a36615d8f
Théodore <theodorechristianradu@gmail.com>
2021-07-02 02:02:59 +0200

    Graphical Twix

--- apps/add_file/forms.py
+++ apps/add_file/forms.py
@@ -4,14 +4,13 @@ from .widgets import SimpleFileInput
 
 class ChoixFichiers(forms.Form):
 
-    parameters = {'text': 'Choisir Fichiers', 'multiple': True, 'upload_to': 'jugements'}
+    parameters = {'text': 'Sélectionner', 'multiple': True, 'upload_to': 'jugements'}
     fichiers = forms.FileField(label='', widget=SimpleFileInput(attrs=parameters))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.id = id(self)
         self.fields['fichiers'].widget.form = self
-        print(self)
 
 
 class ConserverFichier(forms.Form):
