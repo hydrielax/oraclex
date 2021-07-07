dce517f328b9551e244473baa184dc51bb17986b
RandomAnass <Anasss.haut.2020@gmail.com>
2021-07-07 16:30:29 +0200

    update : research

--- codes_python_old/lisible.py
+++ codes_python_old/lisible.py
@@ -13,6 +13,7 @@ except ImportError:
     import Image
 
 import re
+
 import cv2 
 import pytesseract
 from pytesseract import Output
@@ -20,7 +21,7 @@ from pytesseract import Output
 from pdf2image import convert_from_path
 
 from statistics import mean
-from copy import deepcopy
+
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -55,7 +56,6 @@ f = open(myfile,"r")
 
 
 
-
 def reader(s):
     L=[]
     f = open(s,'r', encoding="utf8")
@@ -71,18 +71,31 @@ s='MotsCles.txt'
       
 MotsCles= reader(s)
 
-def search(f,MotsCles):
+def search(txt_path,MotsCles):
     #re.split('; |, |\*|\n',L) possible
-    L=[]
+    """L=[]
+    f = open(txt_path,"r") 
     texte = f.read().replace("\n", " ")
+    texte = re.split('; |, |\*|\n',texte)
     MotsCles = set(MotsCles)
     for mot in texte :
-        print(mot)
+        print(mot, '\n')
         if mot in MotsCles:
             L.append(mot)
+    return L"""
+    L=set()
+    for mot in MotsCles:
+        with open(txt_path, 'r') as inF:
+            for line in inF:
+                if mot in line.upper():
+                    L.add(mot)
+                
     return L
 
-print(search(f,MotsCles))
+
+L=search(myfile,MotsCles)
+print(L)
+
 
 
 ################################################################################################
@@ -140,3 +153,103 @@ def pdf_to_txt(pdfs,txt_path):
                 word_list = []
         with open(txt_path,  'w', newline="") as file:
                   csv.writer(file, delimiter=" ").writerows(parse_text)
+
+
+#########################################################
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 14:32:25 2021
+
+@author: anass
+"""
+
+
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+import re
+
+import cv2 
+import pytesseract
+from pytesseract import Output
+
+from pdf2image import convert_from_path
+
+from statistics import mean
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+
+def pdf_to_txt(pdfs,txt_path):
+    # Adding custom options
+    custom_config = r'--oem 3 --psm 6'
+    pages = convert_from_path(pdfs, 350)
+    i = 1
+    for page in pages:
+        image_name = "Page_" + str(i) + ".jpg" 
+        img = cv2.imread(image_name)
+        #page.save(image_name, "JPEG")
+        i = i+1
+        f = open(txt_path, "a")
+        f.write(pytesseract.image_to_string(img, config=custom_config, lang='fra'))
+        #f.write(pytesseract.image_to_string(Image.open(image_name), lang='fra'))
+        print(mean(list(map(float, pytesseract.image_to_data(Image.open(image_name), output_type = Output.DICT, config=custom_config, lang='fra')['conf']))))
+        f.close()
+
+   
+
+pdfs = r"C:\Users\anass\Programmation\Extract info\input.pdf"  
+txt_path = r"C:\Users\anass\Programmation\Extract info\output2.txt"
+pdf_to_txt(pdfs,txt_path)
+        
+myfile = txt_path
+
+f = open(myfile,"r") 
+
+
+
+
+def reader(s):
+    L=[]
+    f = open(s,'r', encoding="utf8")
+    for line in f:
+        if line != '\n' :
+            newline=line
+            if '\n' in newline :
+                newline=newline.replace('\n','')
+            L.append(newline)
+    return L
+
+s='MotsCles.txt' 
+      
+MotsCles= reader(s)
+
+def search(txt_path,MotsCles):
+    #re.split('; |, |\*|\n',L) possible
+    """L=[]
+    f = open(txt_path,"r") 
+    texte = f.read().replace("\n", " ")
+    texte = re.split('; |, |\*|\n',texte)
+    MotsCles = set(MotsCles)
+    for mot in texte :
+        print(mot, '\n')
+        if mot in MotsCles:
+            L.append(mot)
+    return L"""
+    L=set()
+    for mot in MotsCles:
+        with open(txt_path, 'r') as inF:
+            for line in inF:
+                if mot in line.upper():
+                    L.add(mot)
+                
+    return L
+
+
+L=search(myfile,MotsCles)
+print(L)
