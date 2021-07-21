0b254b820051623eb0101264ce0979e21183239f
RandomAnass <Anasss.haut.2020@gmail.com>
2021-07-21 18:06:09 +0200

    Predict

--- apps/predict/predict_model.py
+++ apps/predict/predict_model.py
@@ -3,11 +3,98 @@ from apps.search.models import Jugement
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.models import load_model
 import random
 import numpy as np
 
-#model = tf.keras.models.load_model("predict.model")
 
-def model_ia(a,b):
-    return a+b
 
+model_file = None
+
+def Get_training_data_decision():
+    Data=[] 
+    for jugement in  Jugement.objects.all() :
+        MotsCles_jugement = jugement.mots_cles.all()
+        decision = jugement.decision
+        in_data = [int(motCle in MotsCles_jugement) for motCle in MotCle.objects.all()] 
+        out_data=[0,0]
+        if decision == 'D':
+            out_data[0]=1
+        elif decision == 'M':
+            continue
+            out_data=0.5
+        elif decision =='F':
+            out_data[1]=1
+        else:
+            print("***",decision)
+            continue
+        print("getting 4")
+        Data.append([in_data,out_data])
+    return Data
+
+def Get_training_data_somme():
+    Data=[] 
+    for jugement in  Jugement.objects.all() :
+        MotsCles_jugement = jugement.mots_cles()
+        jugement.decision = 'F'|'M'|'D'
+        in_data = [int(motCle in MotsCles_jugement) for motCle in MotCle.objects.all()] #in_data = [motCle in MotsCles_jugement for motCle in MotCle.objects.all()]
+        out_data=jugement.gain
+        Data.append([in_data,out_data])
+    return Data
+
+
+
+def model_ia(Xnew):
+    #Xnew.extend([0,0,0])
+    X_input=np.array(Xnew)
+    given_input=np.shape(X_input)[0]
+    X_input=np.asarray(X_input).astype(np.int)
+    global model_file
+    if model_file :
+        print("here")
+        shape_input=model_file.layers[0].input_shape
+        model_input=shape_input[1]
+        print("****")
+        print(given_input,model_input)
+        print("****")
+        if given_input == model_input :
+            print("here1")
+            print(tf.keras.metrics.Accuracy(name="accuracy", dtype=None))
+            output = model_file.predict_proba(np.expand_dims(X_input, axis=0)) 
+            print("here2")
+            return output[0]
+        else:
+            print("training")
+            model_file=train_model()
+            return model_ia(Xnew)
+    else :
+        print("loading")
+        try :
+            model_file = load_model('media/predict/prediction_model')
+        except :
+            model_file=train_model()
+        finally:
+            print("go")
+            return model_ia(Xnew)
+
+
+def train_model():
+    #Bring data:
+    training_data = Get_training_data_decision()
+    random.shuffle(training_data)
+    X=[]
+    y=[]
+    for file_data, categ in training_data :
+        X.append(file_data)
+        y.append(categ)
+    X=np.array(X)
+    y=np.array(y)
+    N=np.shape(X)[1]
+    model_file = tf.keras.models.Sequential()
+    model_file.add(tf.keras.layers.Dense(input_shape=(N,), units=N, activation=tf.nn.relu))
+    model_file.add(tf.keras.layers.Dense(2,activation=tf.nn.softmax))
+    model_file.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+    model_file.fit(X,y,epochs=50, batch_size=32,validation_split=0.25) #to  change
+    model_file.save("media/predict/prediction_model")
+    #print(model.summary())
+    return model_file 
--- apps/predict/views.py
+++ apps/predict/views.py
@@ -1,10 +1,11 @@
 
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from apps.predict.forms import RequeteForm
 from apps.search.models import Jugement, MotCle, Categorie, Mot
-from .predict_model import *
+from .predict_model import model_ia
 
 
 
@@ -21,10 +22,22 @@ def prediction(request):
             var_bool = [int(motCle in motsCles) for motCle in MotCle.objects.all()]
             #proba = ia(variantes)
             proba = model_ia(var_bool)#verify the form first
-            context['proba'] = proba
+            context['proba'] = proba[0]
+            context['dec'] = 'Def' if proba[0]>proba[1] else 'Fav'
     else:
         form = RequeteForm()
     context['form'] = form
     return render(request, 'predict/index.html', context)
 
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class ChartData(APIView):
+  
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        data = {"defavorable": 51, "favorable": 49}
+        return Response(data)
